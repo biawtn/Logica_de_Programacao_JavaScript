f4a0495f037f4cbdef1c8129c80fda0cc55c7505
Banca Noronha <teixeiranoronhabianca@gmail.com>
2026-04-25 17:16:18 -0300

    Create scriptnode.js with commented code example

---
+++ Condições/scriptnode.js
@@ -0,0 +1,22 @@
+//Exemplo 0.1
+// let ouvinte = document.querySelector(".ouvinte")
+// let resSitu = document.querySelector(".resSitu")
+
+// ouvinte.addEventListener("click", (e) => {
+//     e.preventDefault()
+//     let nomeAluno = document.querySelector(".inNome").value
+//     let nota1 = +document.querySelector(".n1").value
+//     let nota2 = +document.querySelector(".n2").value
+
+//     let media = (nota1+nota2)/2
+
+//         if(media>=7){
+//             resSitu.innerHTML = `${nomeAluno} foi Aprovado!`
+//             resSitu.style.color = "green"
+//         } else{
+//             document.querySelector(".resSitu").innerHTML = `${nomeAluno} foi reprovado...`
+//             resSitu.style.color = "red"
+//         }
+
+//     document.querySelector(".resMedia").innerHTML = `A média é de ${media.toFixed(2)}`
+// })
